490c76551fa50875694834ad5abb8e73148e3e3b
Derek Pollard <derek@derekcodes.com>
2023-04-01 00:03:31 -0500

    Fix deploy

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,9 @@
 import type { V2_MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 
-import { useOptionalUser } from "~/utils";
-
 export const meta: V2_MetaFunction = () => [{ title: "Remix Notes" }];
 
 export default function Index() {
-  const user = useOptionalUser();
   return (
     <main className="relative min-h-screen bg-gray-800 sm:flex sm:items-center sm:justify-center">
       <Link
