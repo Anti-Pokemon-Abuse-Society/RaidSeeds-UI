ccc3d7a3179a2a65f506ba716206fec0e0e89245
Derek Pollard <derek@derekcodes.com>
2023-04-06 17:47:28 -0500

    Update main page link

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
   return (
     <main className="relative min-h-screen bg-gray-800 sm:flex sm:items-center sm:justify-center">
       <Link
-        to="/join"
+        to="/search/shiny6"
         className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
       >
         6IV Shiny Seed Search
